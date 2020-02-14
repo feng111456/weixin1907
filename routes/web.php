28d0b9980c8c8e20c9751e7377b695eb8847ff24
panfeng <983660431@qq.com>
2020-02-14 09:52:05 +0800

    微信路由

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/**微信路由 */
+Route::prefix('wechat')->group(function () {
+    Route::get('/index','Wechat\WechatController@index');
+});
