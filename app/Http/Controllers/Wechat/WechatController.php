7fc0b054b2d1e20b5d4c505371b74b73bdc032fb
panfeng <983660431@qq.com>
2020-02-17 17:07:38 +0800

    连接微信服务器

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class WechatController extends Controller
 {
     public function index(){
-    
+        $signature  = $_GET['signature'];
+        $timestamp  = $_GET['timestamp'];
+        $nonce      = $_GET['nonce'];
+        $token = '123456abc';
+        $tmparrat = array($token,$timestamp,$nonce);
+        sotr($tmparrat,SORT_STRING);
+        $impstr = implode($tmparrat);
+        $impstr = sha1($impstr);
+        if($impstr == $signature){
+            echo $_GET['echostr'];
+        }
     }
 }
