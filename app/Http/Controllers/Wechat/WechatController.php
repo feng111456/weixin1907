e1474be27ab712fbac698b7cd731c0ffe0289651
panfeng <983660431@qq.com>
2020-02-17 16:45:25 +0800

    重新写

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 class WechatController extends Controller
 {
     public function index(){
-        echo  $echostr =request()->echostr;die;
-        $data = request();
-        dump($data);
-        echo $data->echostr;
+    
     }
 }
