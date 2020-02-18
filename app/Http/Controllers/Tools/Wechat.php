7e75d6db3d565b52110720920aa6043428063550
panfeng <983660431@qq.com>
2020-02-18 18:03:47 +0800

    从新写

--- app/Http/Controllers/Tools/Wechat.php
+++ app/Http/Controllers/Tools/Wechat.php
@@ -6,24 +6,9 @@ class Wechat
     const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
     /**获取access_token */
     public static function getAccess_token(){
-        // $access_token ='';
-        // $access_token.= file_get_contents('/access_token.txt');
-        // if(empty($access_token)){
-        //     echo "未发现文件";
-        // }
-        // $access_token = json_decode($access_token,true);
-        // if(time()-$access_token['time']>=7000){
-            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
-            .self::appID."&secret=".self::appsecret;
-            $resStr = file_get_contents($url);    
-            $resArr = json_decode($resStr,true);
-            $resArr['time']= time();
-            $resString = json_encode($resArr);
-            file_put_contents('/access_token.txt',$resString);
-            $access_token = file_get_contents('/access_token.txt');
-            $access_token = json_decode($access_token,true);
-            return $access_token['access_token'];
-        // }    
-        // return $access_token['access_token'];
+        $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=
+        ".self::appID."&secret=".self::appsecret;
+        $resStr = file_get_contents($Url);
+        dd($resStr);
     }
 }
