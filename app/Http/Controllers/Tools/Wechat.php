8e031753202c515d8addbd608a5f12cecfc426c0
panfeng <983660431@qq.com>
2020-02-18 18:19:02 +0800

    从新写

--- app/Http/Controllers/Tools/Wechat.php
+++ app/Http/Controllers/Tools/Wechat.php
@@ -6,7 +6,7 @@ class Wechat
     const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
     /**获取access_token */
     public static function getAccess_token(){
-        $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxff7f4f8c33328445&secret=ac636f8bea398c79823344cec41e4cb2";
+        $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".self::appID."&secret=".self::appsecret;
         $res = file_get_contents($Url);
         $resArr = json_decode($res,true);
         return $resArr;
