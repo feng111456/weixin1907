ec1a72928f57f113778c1c082b72890f7e495d2c
panfeng <983660431@qq.com>
2020-02-18 16:25:43 +0800

    获取access_token

--- app/Http/Controllers/Tools/Wechat.php
+++ app/Http/Controllers/Tools/Wechat.php
@@ -5,7 +5,7 @@ class Wechat
     const appID = 'wxff7f4f8c33328445';
     const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
     /**获取access_token */
-    public function getAccess_token(){
+    public static function getAccess_token(){
         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
         .self::appID."&secret=".self::appsecret;
         $resStr = file_get_contents($url);
