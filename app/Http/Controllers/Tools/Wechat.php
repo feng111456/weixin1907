6a6f4aa802116a96e3ca47c1cc294a23e7f31718
panfeng <983660431@qq.com>
2020-02-18 18:16:48 +0800

    从新写

--- app/Http/Controllers/Tools/Wechat.php
+++ app/Http/Controllers/Tools/Wechat.php
@@ -6,10 +6,9 @@ class Wechat
     const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
     /**获取access_token */
     public static function getAccess_token(){
-        $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=
-        ".self::appID."&secret=".self::appsecret;
+        $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxff7f4f8c33328445&secret=ac636f8bea398c79823344cec41e4cb2";
         $res = file_get_contents($Url);
         $resArr = json_decode($res,true);
-        dd($resArr);
+        return $resArr;
     }
 }
