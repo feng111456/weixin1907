0faf21bdac786da5b414f37d742f224f1446446e
panfeng <983660431@qq.com>
2020-02-18 16:16:40 +0800

    获取access_token

---
+++ app/Http/Controllers/Tools/Wechat.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers\Tools;
+class Wechat 
+{
+    const appID = 'wxff7f4f8c33328445';
+    const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
+    /**获取access_token */
+    public function getAccess_token(){
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
+        .self::appID."&secret=".self::appsecret;
+        $resStr = file_get_contents($url);
+        $resArr = json_decode($res,true);
+        return $resArr['access_token'];
+    }
+}
--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers\Wechat;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Tools\Wechat;
 
 class WechatController extends Controller
 {
     public function index(){
-        $signature  = request()->signature;
-        if(!empty($signature)){
-            $timestamp  = request()->timestamp;
-            $nonce      = request()->nonce;
-            $token = '123456abc';
-            $tmparrat = array($token,$timestamp,$nonce);
-            sort($tmparrat,SORT_STRING);
-            $impstr = implode($tmparrat);
-            $impstr = sha1($impstr);
-            if($impstr == $signature){
-                echo $_GET['echostr'];
-            }
-        }else{
-        echo $signature; 
-        }
+        // $signature  = request()->signature;
+        // if(!empty($signature)){
+        //     $timestamp  = request()->timestamp;
+        //     $nonce      = request()->nonce;
+        //     $token = '123456abc';
+        //     $tmparrat = array($token,$timestamp,$nonce);
+        //     sort($tmparrat,SORT_STRING);
+        //     $impstr = implode($tmparrat);
+        //     $impstr = sha1($impstr);
+        //     if($impstr == $signature){
+        //         echo $_GET['echostr'];
+        //     }
+        // }else{
+        // echo $signature; 
+        // }
+        //调用获取access_token方法
+        $access_token = $this->getAccess_token();
+        echo $access_token;
     }
 }
