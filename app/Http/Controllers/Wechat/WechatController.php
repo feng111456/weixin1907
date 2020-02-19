592e105d4cddece9521c35a89643bb7ea3de5ee8
panfeng <983660431@qq.com>
2020-02-19 19:16:29 +0800

    被动回复

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -26,7 +26,7 @@ class WechatController extends Controller
         //调用获取access_token方法
         //$access_token =Wechat::getAccess_token(); 
         $xml=file_get_contents('php://input');
-        $xml = file_put_contents('check.txt',"\n".$xml,FILE_APPEND);
+        file_put_contents('check.txt',"\n".$xml,FILE_APPEND);
         $xmlObj = simplexml_load_string($xml);
         if($xmlObj->MsgType=='event'){
             //说明是事件 在判断什么是事件
