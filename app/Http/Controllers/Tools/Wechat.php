80c5a2c0e445a15d1ef17b3ad9e764d8fd6fb703
panfeng <983660431@qq.com>
2020-02-25 20:09:05 +0800

    发送模板消息入库

--- app/Http/Controllers/Tools/Wechat.php
+++ app/Http/Controllers/Tools/Wechat.php
@@ -8,13 +8,13 @@ class Wechat
     const appsecret = 'ac636f8bea398c79823344cec41e4cb2';
     /**获取access_token */
     public static function getAccess_token(){
-        $access_token = Cache::get('access_token');
+        $access_token = Cache::get('access_token1');
         if(empty($access_token)){
             $Url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".self::appID."&secret=".self::appsecret;
             $res = file_get_contents($Url);
             $resArr = json_decode($res,true);
-            Cache::put('access_token',$resArr['access_token'],7000);
-            $access_token = Cache::get('access_token');
+            Cache::put('access_token1',$resArr['access_token'],7000);
+            $access_token = Cache::get('access_token1');
             return $access_token;
         }
         return $access_token;
