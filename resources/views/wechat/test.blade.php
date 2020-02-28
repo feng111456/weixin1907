cd55ff50fb150b39798369a9cf09afbb63749e07
panfeng <983660431@qq.com>
2020-02-28 20:51:05 +0800

    测试js

--- resources/views/wechat/test.blade.php
+++ resources/views/wechat/test.blade.php
@@ -13,7 +13,6 @@
 </html>
 
 <script>
-    alert(111);
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wxff7f4f8c33328445', // 必填，公众号的唯一标识
@@ -32,8 +31,8 @@
             desc: '分享给朋友', // 分享描述
             link: 'http://weixin07.zhangpanfeng.top/wechat/test', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: '', // 分享图标
-            success: function () {
-                alert(111);
+            success:function(){
+                alert('OK');
             }
         })
     });
