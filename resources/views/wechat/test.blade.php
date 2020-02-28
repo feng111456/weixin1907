a4590defcd5804264d7f5102be8386a497d37613
panfeng <983660431@qq.com>
2020-02-28 21:42:14 +0800

    测试js

--- resources/views/wechat/test.blade.php
+++ resources/views/wechat/test.blade.php
@@ -16,7 +16,6 @@
 <script>
     var sign= document.getElementById("sign").value;
     alert(sign);
-    return 111;
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wxff7f4f8c33328445', // 必填，公众号的唯一标识
