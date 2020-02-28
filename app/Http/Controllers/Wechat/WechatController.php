881f9dd6f6505047eb4ef496816a6b1dbc3ab151
panfeng <983660431@qq.com>
2020-02-28 09:01:18 +0800

    连接测试

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -11,20 +11,20 @@ use App\Model\Send;
 class WechatController extends Controller
 {
     public function index(){
-        $signature  = request()->signature;
-        if(!empty($signature)){
-            $timestamp  = request()->timestamp;
-            $nonce      = request()->nonce;
-            $token = '123456abc';
-            $tmparrat = array($token,$timestamp,$nonce);
-            sort($tmparrat,SORT_STRING);
-            $impstr = implode($tmparrat);
-            $impstr = sha1($impstr);
-            if($impstr == $signature){
-                echo $_GET['echostr'];
-                 //连接测试成功
-            } 
-        }
+        // $signature  = request()->signature;
+        // if(!empty($signature)){
+        //     $timestamp  = request()->timestamp;
+        //     $nonce      = request()->nonce;
+        //     $token = '123456abc';
+        //     $tmparrat = array($token,$timestamp,$nonce);
+        //     sort($tmparrat,SORT_STRING);
+        //     $impstr = implode($tmparrat);
+        //     $impstr = sha1($impstr);
+        //     if($impstr == $signature){
+        //         echo $_GET['echostr'];
+        //          //连接测试成功
+        //     } 
+        // }
         //调用获取access_token方法
         //$access_token =Wechat::getAccess_token(); 
         //$wechatIp = Wechat::getWechatIp();
